4b86166f5f48fd72c0239359e3d48c6dcf519b92
TonyStark0111 <yedhuku2002@gmail.com>
2025-07-29 13:55:22 +0530

    Update config.py
    Jjj

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "821215952")) # Owner id
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
 DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "XFLIX")
+DB_NAME = os.environ.get("DATABASE_NAME", "MovieSeries")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/CodeflixSupport")
